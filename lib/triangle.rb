01fc7c0410a89d5391ed525fd9c16a16dd51d566
Nicholas Ng <nicholasykng@gmail.com>
2019-11-03 03:53:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,10 +12,10 @@ class Triangle
     else
       if @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
-      elsif 
+      elsif
         @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
         :isosceles
-      elsif 
+      elsif
         @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
         :scalene
       end
@@ -26,4 +26,3 @@ class Triangle
       "The sume of the lengths of any two sides of a triangle always exceeds the length of the third side. Each side must be larger than 0."
 end
 end
-
